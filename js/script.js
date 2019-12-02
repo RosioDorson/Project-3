fdc8e1909fcd7bd27a67ae88e9c51ba7ddc163d1
Rosio <rosioilene@yahoo.com>
2019-12-01 23:11:20 -0800

    Created change event handler

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
-const $jobTitle = $('#title')
-const $otherJob = $('#other-title')
+/******************************************
+Treehouse Techdegree:
+FSJS project 3 - Interactive Form
+Rosio Dorson
+******************************************/
 
+const $jobTitle = $('#title');
+const $otherJob = $('#other-title');
+const $options = $('#color option');
 
-//link the script file to the HTML and check that they are working by logging it to the console.
-console.log('hello');
 //add a focus to the name input field.
 $('#name').focus();
 
-
 //hide the your job roles text field so that it is hidden initially.
 $('#other-title').hide();
+
 //include a text field that shows up when the other option is selected from the job role drop down menu
 $jobTitle.change(function () {
     const change = $jobTitle.find(':selected').text();
@@ -17,21 +21,45 @@ $jobTitle.change(function () {
         $otherJob.show();
     }
 })
-// $('#title').change(function () {
-//     if $('#title').val()=== 'Other' {
-//         $('#other-title').show();
-//     } else {
-//         $('#other-title').hide();
-//     }
-// })
-//until a theme is selected from the design menu no color color options appear and the color field ready "please select a tshirt theme"
 
-//once a design is chosen only the color options for that design should be shown 
+$(document).ready(function() {
+//hide the 'select theme' option in the dropdown menu
+$('#design option:first').hide();
 
-//
-//
-//
-//
-//
-//
-//
+//hide the colors in the dropdown menu
+$options.hide();
+
+//add text to the color field that says "Please select a T-Shirt theme"
+$('#color').prepend("<option>Please select a T-Shirt theme</option>");
+});
+
+//use a conditional stmt inside change event listener to listen for changes made to "design" menu 'select' element
+$('#design').on('change', function() {
+if () {
+
+} else if () {
+
+}
+});
+//create an element to display the total activity cost
+
+//use a change event listener to listen for changes in the activity section
+
+//create variables to store the important values
+
+//update the display and total activity cost 
+
+//disable the conflicting activities
+
+//hide the select "payment method" 'option' so it doesnt show in the dropdown menu
+
+//get the value of the payment select element
+
+//show the corresponding payment option and hide the others
+
+//create a separate validation function for each of the form fields 
+
+//create the master validation function
+
+//add a submit event listener
+
